d23714a54cda22108e182e616e3d41d4e87c597d
Set <sethalberdier@gmail.com>
2020-11-28 14:34:07 -0300

    Finished modal window

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,15 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
 import StickyHeader from './modules/StickyHeader.js';
 import LogoJump from './modules/LogoAnchorJumping.js';
-import RevealOnScroll from './modules/RevealOnScroll';
+import RevealOnScroll from './modules/RevealOnScroll.js';
+import Modal from './modules/Modal.js';
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 80);
 new RevealOnScroll(document.querySelectorAll('.testimonial-item'), 85, 95);
 new MobileMenu;
 new StickyHeader;
 new LogoJump;
+new Modal;
 
 if (module.hot) {
     module.hot.accept();
